cc0f3f17647a33dabefd7b10ad2ef5be7c234ae6
jizhou16 <jizhou16@COE-CS-pc9.ou.ad3.ucdavis.edu>
2025-05-11 15:08:30 -0700

    initial queue data structure

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -6,11 +6,31 @@
 
 struct queue {
 	/* TODO Phase 1 */
+
+	/* 
+	 * If we need O(1) operations, the best data structure for this
+	 * would probably be a linked list, since we don't need access
+	 * to the middle elements of the list.
+	 */
+
+	node_t *first;
+	node_t *last
+	int length;
 };
+
+typedef struct queue_node {
+	void *data;
+	node_t *next;
+} node_t;
 
 queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
+	node_t *node_ptr = malloc(sizeof(node_t));
+	queue_t *queue_ptr = malloc(sizeof(queue_t));
+	queue_ptr->first = node_ptr;
+	queue_ptr->last = node_ptr;
+	queue_ptr->length = 1;
 }
 
 int queue_destroy(queue_t queue)
